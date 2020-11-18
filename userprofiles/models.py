6e72c1950eb16ed7bf35f367f630ebfeaa54447a
Aurelien Moreau <aurelien.moreau@angus.ai>
2020-11-18 12:37:08 +0100

    Add a serializers for user profile, update profiles to be use with an API

--- userprofiles/models.py
+++ userprofiles/models.py
@@ -1,5 +1,8 @@
+from uuid import uuid4
 from django.contrib.auth.models import User
 from django.db import models
+
+from rest_framework.authtoken.models import Token
 
 
 class Team(models.Model):
@@ -19,8 +22,14 @@ class UserProfile(models.Model):
     Profile is a new table to extend native django user.
 
     """
+    # UUID is a good handler for API manipulation and data migration
+    uid = models.UUIDField(primary_key=True, default=uuid4)
     # we authorize a profile without team
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    # Each user can be access to its data with a API key
+    # Good for a frontend or an external web service.
+    token = models.ForeignKey(Token, on_delete=models.SET_NULL, null=True, blank=True)
     team = models.ForeignKey(
         Team,
         on_delete=models.PROTECT,
---
+++ userprofiles/serializers.py
@@ -0,0 +1,71 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from . import models
+from rest_framework.authtoken.models import Token
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """
+    Serializer for a native django user object
+    """
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'email',
+            'first_name',
+            'last_name'
+            'password'
+        )
+        extra_kwargs = {
+            'password': {
+                'write_only': True
+            }
+        }
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    """
+    Serializer for a user profile object and
+    django user native object
+    """
+    user = UserSerializer()
+
+    class Meta:
+        model = models.UserProfile
+        fields = (
+            'uid',
+            'user',
+            'team',
+            'token',
+        )
+        extra_kwargs = {
+            'uid': {
+                'read_only': True
+            },
+            'token': {
+                'read_only': True
+            }
+        }
+
+    def create(self, validated_data):
+        """
+        Override default serializer create function to
+        create django user, token and set the password
+        """
+        user_data = validated_data.pop('user')
+
+        password = user_data.pop('password')
+        user = User(**user_data)
+        user.set_password(password)
+        user.save()
+
+        token = Token.objects.create(user=user)
+
+        profile = models.UserProfile.objects.create(
+            user=user,
+            token=token,
+            **validated_data
+        )
+
+        return profile
