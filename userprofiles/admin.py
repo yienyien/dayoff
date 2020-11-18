19947da712ab444384390ff81c3f3fee2d540117
Aurelien Moreau <aurelien.moreau@angus.ai>
2020-11-18 12:15:00 +0100

    Define user (employee) profile and team

--- userprofiles/admin.py
+++ userprofiles/admin.py
@@ -1,3 +1,16 @@
+"""
+Record object into the administration dashboard
+"""
+
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+@admin.register(models.UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Team)
+class TeamAdmin(admin.ModelAdmin):
+    pass
--- userprofiles/models.py
+++ userprofiles/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Team(models.Model):
+    """
+    Team is the representation of a compagny team.
+    All employees belong to a team. There can be many teams.
+    """
+    slugname = models.CharField(max_length=5, primary_key=True)
+    fullname = models.CharField(max_length=256)
+
+    description = models.TextField()
+    icones = models.ImageField()
+
+
+class UserProfile(models.Model):
+    """
+    Profile is a new table to extend native django user.
+
+    """
+    # we authorize a profile without team
+    team = models.ForeignKey(Team, on_delete=models.PROTECT, null=True, blank=True)
