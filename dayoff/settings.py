20bb6438698a7969033dec1f594357f743b6dc50
Aurelien Moreau <aurelien.moreau@angus.ai>
2020-11-18 12:04:44 +0100

    Configure Django Rest Framework

--- dayoff/settings.py
+++ dayoff/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
     'userprofiles',
 ]
 
@@ -119,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100
+}
